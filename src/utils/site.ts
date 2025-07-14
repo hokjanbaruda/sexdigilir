2f1a5619797eeb03589a7e537abd0c199c1c9a53
hokjanbaruda <hokjanbaruda@gmail.com>
2025-07-14 21:52:49 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Sex Digilir';
-export const desk = 'Video Bokep Indo Jepang Jav Sex Gilir Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma';
-export const url = 'https://sexdigilir.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
